a39468427397f3accc69385e677a8da1b74a9cda
Chandra Shekhar <shekharait254@gmail.com>
2021-01-26 15:30:24 +0530

    add anotate gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id            :bigint           not null, primary key
+#  name          :string
+#  email         :string
+#  status        :string
+#  mobile_number :string
+#  role_id       :bigint           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
 class User < ApplicationRecord
   belongs_to :role
 end
